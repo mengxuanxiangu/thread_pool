f59e68bab6e2bb1d73fddb7c0049b9da5d66d12d
mengxuanxiangu <houjinchao@gmail.com>
2016-07-11 21:03:06 +0800

    first

---
+++ src/main.cpp
@@ -0,0 +1,22 @@
+/***************************************************************************
+ * 
+ * Copyright (c) 2016 mengxuanxiangu, Inc. All Rights Reserved
+ * $Id$ 
+ * 
+ **************************************************************************/
+ 
+ /**
+ * @file main.cpp
+ * @author mengxuanxiangu(mengxuanxiangu@gmail.com)
+ * @date 2016/07/11 21:00:29
+ * @version $Revision$ 
+ * @brief 
+ *  
+ **/
+#include <iostream>
+
+int main() {
+    std::cout << "hello world" << std::endl;
+}
+
+/* vim: set ts=4 sw=4 sts=4 tw=100 */
